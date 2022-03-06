9e361192e42e13cf36b3f718d746d959d86ae0cb
ErshovN <99174712+ErshovN@users.noreply.github.com>
2022-03-06 12:07:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,47 @@
 
 
 double pown(double value, uint16_t n) {
-  jvjhv
+  if (n == 0) {
+		return 1;
+	}
+	else {
+		return value * pown(value, n - 1);
+	}
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n == 0) {
+		return 1;
+	}
+	else {
+		return n * fact(n - 1);
+	}
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return (pow(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+  double e = 0;
+	for (uint16_t i = 0; i <= count; i++) {
+		e += (pown(x, i)) / fact(i);
+	}
+	return e;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double sin = 0;
+	for (uint16_t i = 1; i <= count; ++i) {
+		sin += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+		return sin;
+	}
 }
 
 double cosn(double x, uint16_t count) {
-
+  double cos = 0;
+	for (uint16_t i = 1; i <= count; ++i) {
+		cos += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+	}
+	return cos;
 }
